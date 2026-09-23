19423e93ea81e18cf24a58caf3b8dc6b86457f63
andreeuss <perdomovillamilhectorandres@gmail.com>
2026-09-23 10:30:23 -0500

    Actualizar caché PWA a v4.1.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='coordinador-vibraciones-v4.0.0';
-const ASSETS=['./','./index.html','./app.js','./chapters.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='coordinador-vibraciones-v4.1.0';
+const ASSETS=['./','./index.html','./app.js','./v4_1.js','./chapters.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();if(response.ok&&new URL(event.request.url).origin===self.location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});

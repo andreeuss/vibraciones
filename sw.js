fe2307986a8c55041a9f07fccdca930def694147
andreeuss <perdomovillamilhectorandres@gmail.com>
2026-09-23 10:57:34 -0500

    Actualizar caché PWA v4.1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='coordinador-vibraciones-v4.1.0';
+const CACHE='coordinador-vibraciones-v4.1.1';
 const ASSETS=['./','./index.html','./app.js','./v4_1.js','./chapters.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
